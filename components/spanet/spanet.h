2818842446284a4ce69c79b6b06b47e17440ce51
SCUZNUTS <PATRICK.alpe@gmail.com>
2023-09-16 19:50:16 +1200

    Update spanet.h

--- components/spanet/spanet.h
+++ components/spanet/spanet.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "esphome/core/component.h"
-//#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/sensor/sensor.h"
+//#include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/uart/uart.h"
 
